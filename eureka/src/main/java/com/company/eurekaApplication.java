5695e441fc2a3b39e013267f411f1faa81c92c3d
lz_shawn <1360069912@qq.com>
2021-05-31 10:30:08 +0800

    ttt

--- eureka/src/main/java/com/company/eurekaApplication.java
+++ eureka/src/main/java/com/company/eurekaApplication.java
@@ -1,15 +1,20 @@
 package com.company;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaServer
+@Slf4j
 public class eurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(eurekaApplication.class, args);
+        SpringApplication app = new SpringApplication(eurekaApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功");
+        log.info("Eureka地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
-
 }
