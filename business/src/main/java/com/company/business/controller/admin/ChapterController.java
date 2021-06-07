ec812467cf96b5ce1497e208a94898b6533dab3e
lz_shawn <1360069912@qq.com>
2021-06-07 15:36:29 +0800

    1.增加日志AOP，统一日志输出 2.logback增加打印日志跟踪号

--- business/src/main/java/com/company/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/company/business/controller/admin/ChapterController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/chapter")
 @Slf4j
 public class ChapterController {
+    public static final String BUSINESS_NAME = "大章";
 
     @Autowired
     private ChapterService chapterService;
