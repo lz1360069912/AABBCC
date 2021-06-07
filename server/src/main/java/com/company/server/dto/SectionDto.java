0fe06a86aa1a017b91eb2e5937878f6037d87b8e
lz_shawn <1360069912@qq.com>
2021-06-07 22:01:08 +0800

    1.生成器开发：增加dto生成

--- server/src/main/java/com/company/server/dto/SectionDto.java
+++ server/src/main/java/com/company/server/dto/SectionDto.java
@@ -1,27 +1,62 @@
 package com.company.server.dto;
 
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class SectionDto {
+
+    /**
+     * id
+     */
     private String id;
 
+    /**
+     * 标题
+     */
     private String title;
 
+    /**
+     * 课程|course.id
+     */
     private String courseId;
 
+    /**
+     * 大章|chapter.id
+     */
     private String chapterId;
 
+    /**
+     * 视频
+     */
     private String video;
 
+    /**
+     * 时长|单位秒
+     */
     private Integer time;
 
+    /**
+     * 收费|C 收费；F 免费
+     */
     private String charge;
 
+    /**
+     * 顺序
+     */
     private Integer sort;
 
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createdAt;
 
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updatedAt;
+
 
     public String getId() {
         return id;
@@ -30,7 +65,6 @@ public class SectionDto {
     public void setId(String id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
@@ -38,7 +72,6 @@ public class SectionDto {
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getCourseId() {
         return courseId;
     }
@@ -46,7 +79,6 @@ public class SectionDto {
     public void setCourseId(String courseId) {
         this.courseId = courseId;
     }
-
     public String getChapterId() {
         return chapterId;
     }
@@ -54,7 +86,6 @@ public class SectionDto {
     public void setChapterId(String chapterId) {
         this.chapterId = chapterId;
     }
-
     public String getVideo() {
         return video;
     }
@@ -62,7 +93,6 @@ public class SectionDto {
     public void setVideo(String video) {
         this.video = video;
     }
-
     public Integer getTime() {
         return time;
     }
@@ -70,7 +100,6 @@ public class SectionDto {
     public void setTime(Integer time) {
         this.time = time;
     }
-
     public String getCharge() {
         return charge;
     }
@@ -78,7 +107,6 @@ public class SectionDto {
     public void setCharge(String charge) {
         this.charge = charge;
     }
-
     public Integer getSort() {
         return sort;
     }
@@ -86,7 +114,6 @@ public class SectionDto {
     public void setSort(Integer sort) {
         this.sort = sort;
     }
-
     public Date getCreatedAt() {
         return createdAt;
     }
@@ -94,7 +121,6 @@ public class SectionDto {
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
-
     public Date getUpdatedAt() {
         return updatedAt;
     }
@@ -122,4 +148,5 @@ public class SectionDto {
         sb.append("]");
         return sb.toString();
     }
+
 }
