e53089d0aac368ea84c60c2a2f382eb30b505ff5
lz_shawn <1360069912@qq.com>
2021-06-14 22:19:24 +0800

    分类表设计和基本代码生成

---
+++ business/src/main/java/com/company/business/controller/admin/CategoryController.java
@@ -0,0 +1,64 @@
+package com.company.business.controller.admin;
+
+import com.company.server.dto.CategoryDto;
+import com.company.server.dto.PageDto;
+import com.company.server.dto.ResponseDto;
+import com.company.server.service.CategoryService;
+import com.company.server.util.ValidatorUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+public class CategoryController {
+    public static final String BUSINESS_NAME = "分类";
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 列表查询
+     * @param pageDto
+     * @return
+     */
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto) {
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     * @param categoryDto
+     * @return
+     */
+    @PostMapping("/save")
+    public ResponseDto save(@RequestBody CategoryDto categoryDto) {
+        // 保存校验
+        ValidatorUtil.require(categoryDto.getParent(), "父id");
+        ValidatorUtil.require(categoryDto.getName(), "名称");
+        ValidatorUtil.length(categoryDto.getName(), "名称", 1, 50);
+
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.save(categoryDto);
+        responseDto.setContent(categoryDto);
+        return responseDto;
+    }
+
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/delete/{id}")
+    //@RequestBody接收流数据
+    public ResponseDto delete(@PathVariable String id) {
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.delete(id);
+        return responseDto;
+    }
+}
