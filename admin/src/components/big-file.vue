8b2ba0ecc549411f6224c17182a7e5f4216013cc
lz_shawn <1360069912@qq.com>
2021-07-04 11:14:53 +0800

    1.大文件断点续传与极速妙传：修复分片上传size获取不到bug

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -37,7 +37,6 @@ export default {
   methods: {
     uploadFile() {
       let _this = this;
-      let formData = new window.FormData();
       let file = _this.$refs.file.files[0];
 
       // 生成文件标识，标识多次上传的是不是同一个文件
@@ -45,7 +44,7 @@ export default {
       var key10 = parseInt(key, 16);
       var key62 = Tool._10to62(key10);
 
-      //判断文件格式
+      // 判断文件格式
       let suffixs = _this.suffixs;
       let fileName = file.name;
       let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
@@ -63,63 +62,63 @@ export default {
       }
 
       // 文件分片
-      let shardSize = 5 * 1024 * 1024; // 以20MB为一个分片
+      let shardSize = 10 * 1024 * 1024; // 以10MB为一个分片
       let shardIndex = 1; // 分片索引
-      let start = (shardIndex - 1) * shardSize; // 当前分片起始位置
-      let end = Math.min(file.size, start + shardSize); // 当前分片结束位置
-      let fileShard = file.slice(start, end); // 从文件中截取当前的分片数据
       let size = file.size;
       let shardTotal = Math.ceil(size / shardSize); // 总片数
 
-      /*// key:"file"必须和后端controller参数名一致
-      formData.append("shard", fileShard);
-      formData.append("shardIndex", shardIndex);
-      formData.append("shardSize", shardSize);
-      formData.append("shardTotal", shardTotal);
-      formData.append("use", _this.use);
-      formData.append("name", fileName);
-      formData.append("suffix", suffix);
-      formData.append("size", size);
-      formData.append("key", key62);
+      let param = {
+        "shardIndex": shardIndex,
+        "shardSize": shardSize,
+        "shardTotal": shardTotal,
+        "use": _this.use,
+        "name": fileName,
+        "suffix": suffix,
+        "size": size,
+        "key": key62
+      };
 
-      Loading.show();
-      _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload", formData).then((response) => {
-        Loading.hide();
-        let resp = response.data;
-        _this.afterUpload(resp);
-      });*/
-
+      _this.upload(param);
+    },
+    selectFile() {
+      let _this = this;
+      $("#" + _this.inputId + "-input").trigger("click");
+    },
+    upload(param) {
+      let _this = this;
+      let shardIndex = param.shardIndex;
+      let shardTotal = param.shardTotal;
+      let shardSize = param.shardSize;
+      let fileShard = _this.getFileShard(shardIndex, shardSize);
       // 将图片转为base64进行传输 读数据时就会触发onload
       let fileReader = new FileReader();
       fileReader.onload = function (e) {
         let base64 = e.target.result;
-        console.log("base64", base64);
-        let param = {
-          "shard": base64,
-          "shardIndex": shardIndex,
-          "shardSize": shardSize,
-          "shardTotal": shardTotal,
-          "use": _this.use,
-          "name": fileName,
-          "suffix": suffix,
-          "size": size,
-          "key": key62,
-        };
+        param.shard = base64;
 
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload", param).then((response) => {
           Loading.hide();
           let resp = response.data;
-          _this.afterUpload(resp);
+          if (shardIndex < shardTotal) {
+            // 上传下一个分片
+            param.shardIndex = param.shardIndex + 1;
+            _this.upload(param);
+          } else {
+            _this.afterUpload(resp);
+            $("#" + _this.inputId + "-input").val("");
+          }
         });
-
-      }
-
+      };
       fileReader.readAsDataURL(fileShard);
     },
-    selectFile() {
+    getFileShard(shardIndex, shardSize) {
       let _this = this;
-      $("#" + _this.inputId + "-input").trigger("click");
+      let file = _this.$refs.file.files[0];
+      let start = (shardIndex - 1) * shardSize; // 当前分片起始位置
+      let end = Math.min(file.size, start + shardSize); // 当前分片结束位置
+      let fileShard = file.slice(start, end); // 从文件中截取当前的分片数据
+      return fileShard;
     }
   }
 }
