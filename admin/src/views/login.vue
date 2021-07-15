96e040003d6ee10841b5b53888dba4e5a8383665
lz_shawn <1360069912@qq.com>
2021-07-15 13:07:17 +0800

    1.用户管理与登录：增加记住登录信息功能

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -40,7 +40,7 @@
 
                         <div class="clearfix">
                           <label class="inline">
-                            <input type="checkbox" class="ace"/>
+                            <input v-model="remember" type="checkbox" class="ace"/>
                             <span class="lbl"> 记住我</span>
                           </label>
 
@@ -72,16 +72,25 @@ export default {
   name: "login",
   data: function () {
     return {
-      user: {} // 用于绑定form表单的数据
+      user: {}, // 用于绑定form表单的数据
+      remember: true // 默认勾选记住我
     }
   },
   mounted: function () {
+    let _this = this;
     $("body").removeClass("no-skin");
     $("body").attr("class", "login-layout light-login");
+
+    // 从缓存中获取记住的用户名密码，如果获取不到，说明上一次没有勾选“记住我“
+    let rememberUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER);
+    if (rememberUser) {
+      _this.user = rememberUser;
+    }
   },
   methods: {
     login() {
       let _this = this;
+      let passwordShow = _this.user.password;
       _this.user.password = hex_md5(_this.user.password + KEY);
 
       Loading.show();
@@ -90,7 +99,20 @@ export default {
         let resp = response.data;
         if (resp.success) {
           console.log("登录成功：{}", resp.content);
+          let loginUser = resp.content;
           Tool.setLoginUser(resp.content);
+
+          // 判断”记住我“
+          if (_this.remember) {
+            // 如果勾选”记住我“，则将用户名密码保存到本地缓存，这里需要保存明文密码，否则登陆时又会再加一层密
+            LocalStorage.set(LOCAL_KEY_REMEMBER_USER, {
+              loginName: loginUser.loginName,
+              password: passwordShow
+            });
+          } else {
+            // 没有勾选”记住我“时，要把本地缓存清空，否则按照mounted的逻辑，下次打开时会自动显示用户名密码
+            LocalStorage.set(LOCAL_KEY_REMEMBER_USER, null);
+          }
           _this.$router.push("/welcome")
         } else {
           Toast.warning(resp.message);
